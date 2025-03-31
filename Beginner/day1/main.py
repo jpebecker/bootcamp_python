f20688d1bddda5b6129f7a3640db78543322827b
Becker <jpebecker@gmail.com>
2025-03-30 21:33:34 -0300

    day1 task for the bootcamp and gitignore

---
+++ Beginner/day1/main.py
@@ -0,0 +1,7 @@
+print("Bem vindo ao gerador de nome de bandas de rock alternativo contemporâneo")
+
+city = input('Digite o nome da cidade em que cresceu: \n').capitalize()
+petName = input('Digite o nome de seu pet: \n').capitalize()
+
+print(f'o nome de sua banda poderia ser {city} {petName}')
+
