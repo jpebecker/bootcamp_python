b6cfdbe0c02c4fd05fff816f2f11000bf6bb90f0
Becker <jpebecker@gmail.com>
2025-04-13 15:17:34 -0300

    day30 - JSON convertion of day29 application

--- Intermediate/day26/main.py
+++ Intermediate/day26/main.py
@@ -1,12 +1,20 @@
 import pandas
 dataframe_test = pandas.DataFrame(pandas.read_csv('nato_alphabet.csv').to_dict())
-entry = input('Digite uma palavra para receber sua tradução fonética em NATO: ').strip().upper()
 answerList = []
-for l in entry:
-    code = dataframe_test.loc[dataframe_test['letter'] == l, 'code']
-    if not code.empty:
-        answerList.append(code.values[0])
-    else:
-        answerList.append(f"[?{l}]")
+def translate():
+    entry = input('Digite uma palavra para receber sua tradução fonética em NATO: ').strip().upper()
 
-print(answerList)
+    for l in entry:
+        code = dataframe_test.loc[dataframe_test['letter'] == l, 'code']
+        if not code.empty:
+            answerList.append(code.values[0])
+        else:
+            print('*\n'*10)
+            print('Valor inserido não faz parte do alfabeto!')
+            return
+
+    print(answerList)
+
+while True:
+    translate()
+    answerList.clear()
--- Intermediate/day29-30/main.py
+++ Intermediate/day29-30/main.py
@@ -1,12 +1,21 @@
 from tkinter import *
 from tkinter import messagebox
-import os,random
+import random,json,os
 FONT = ('Times New Roman', 12, 'bold')
+################################################# Caminho para desktop
+desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+file_path = os.path.join(desktop_path, "passwords.json")
 #######################################################################FUNCTIONS
 def save_values():
-    adress = website_entrybox.get()
+    adress = website_entrybox.get().lower()
     user = user_entrybox.get()
     passw = pass_entrybox.get()
+
+    new_data = {adress: {
+                'username' : user,
+                'password' : passw,
+        }
+    }
 
     if len(adress) == 0 or len(user) == 0 or len(passw) == 0:
         messagebox.showerror(title='Erro de Entrada',message='Dados Incompletos!')
@@ -19,21 +28,36 @@ def save_values():
 
         if confirmation:
             messagebox.showinfo(title='Confirmação',message='Dados Salvos')
-            desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-            filepath = os.path.join(desktop_path, "passwords.txt")
 
-            if os.path.isfile(filepath):  #se existir arquivo salvo na desktop
-                with open(filepath, mode='a') as newFile:
-                    newFile.write(f'\n>>> {adress} | {user} | {passw}')
-                    clear_all()
-            else:  #se nao existir arquivo salvo na desktop
-                with open(filepath, mode='w') as newFile:
-                    newFile.write(f'Plataforma | Login | Senha\n')
-                    newFile.write('---------------------------')
-                    newFile.write(f'\n>>> {adress} | {user} | {passw}')
-                    clear_all()
+            try: #tenta abrir o arquivo
+                with open(file_path, "r") as data_file:
+                    data = json.load(data_file)
+                    data.update(new_data)
+                    with open(file_path, "w") as data_fileA:
+                        json.dump(data, data_fileA, indent=4)
+            except FileNotFoundError: #se nao encontrar
+                with open(file_path,"w") as newFile:############escreve um novo arquivo
+                    json.dump(new_data,newFile,indent=4)
+            finally:
+                clear_all()
         else:
             clear_all()
+
+def load_values():
+    try:  # tenta abrir o arquivo
+        with open(file_path, "r") as data_file:
+            data = json.load(data_file)
+            if website_entrybox.get().lower() in data.keys():
+                messagebox.showinfo(title='Retorno de Consulta',message=f'Dados Recuperados:\n'
+                                                                        f'Plataforma: {website_entrybox.get()}\n'
+                                                                        f'Login: {data[website_entrybox.get()]['username']}\n'
+                                                                        f'Senha: {data[website_entrybox.get()]['password']}\n')
+            else:
+                messagebox.showwarning(title='Erro de Consulta', message='Registro não encontrado no arquivo')
+    except FileNotFoundError:  # se nao encontrar
+        messagebox.showwarning(title='Erro de Consulta',message='Arquivo não encontrado na Desktop')
+    finally:
+        clear_all()
 
 def clear_all():
     website_entrybox.delete(0,END)
@@ -58,6 +82,7 @@ def create_password():
     password = "".join(password_list)
     pass_entrybox.delete(0,END)
     pass_entrybox.insert(END,password)
+
 #############################################################################USER INTERFACE
 tela = Tk()
 tela.config(padx=50, pady=50)
@@ -66,32 +91,38 @@ canvas = Canvas(width=200, height=200, highlightthickness=0)
 canvas.grid(column=1, row=0)
 img = PhotoImage(file='icon.png')
 canvas.create_image(100, 100, image=img)
-
+###################################################################
 website_text = Label(text='Website/Platform:', font=FONT)
 website_text.grid(column=0, row=1)
-website_entrybox = Entry(width=40)
-website_entrybox.focus()
-website_entrybox.grid(column=1, row=1, columnspan=2, sticky='w')
 
+website_frame = Frame(tela)
+website_frame.grid(column=1, row=1, sticky='w')
+
+website_entrybox = Entry(website_frame,width=25)
+website_entrybox.focus()
+website_entrybox.grid(column=1, row=1,padx=2)
+website_seachbtn = Button(website_frame,text='Search',width=10,padx=5,command=load_values)
+website_seachbtn.grid(column=2,row=1,sticky='w')
+#######################################################################
 user_text = Label(text='Username/E-mail:', font=FONT)
 user_text.grid(column=0, row=2)
 user_entrybox = Entry(width=40)
 user_entrybox.insert(END,'@gmail.com')
 user_entrybox.grid(column=1, row=2, columnspan=2, sticky='w')
-
+#########################################################################
 passw_text = Label(text='Password:', font=FONT)
-passw_text.grid(column=0, row=3)
+passw_text.grid(column=0, row=3,sticky='w')
 
-# Frame alinhado
 pass_frame = Frame(tela)
 pass_frame.grid(column=1, row=3, sticky='w')
 
-pass_entrybox = Entry(pass_frame, width=21)
+pass_entrybox = Entry(pass_frame, width=22)
 pass_entrybox.grid(column=0, row=0)
 
 generatepassword_btn = Button(pass_frame, text='Generate Password',command=create_password)
-generatepassword_btn.grid(column=1, row=0, padx=5)
+generatepassword_btn.grid(column=1, row=0, padx=1)
 
 add_tolist_btn = Button(text='Add to Local Desktop', width=34,command=save_values)
-add_tolist_btn.grid(column=1, row=4, columnspan=2, sticky='w')
+add_tolist_btn.grid(column=1, row=4, columnspan=2, sticky='w',pady=5)
+#####################################################################################################
 tela.mainloop()
